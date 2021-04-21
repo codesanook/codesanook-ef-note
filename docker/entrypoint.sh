48c63d07c8c9704846294e502c68d6689d6c23cb
aaronamm <theeranitp@gmail.com>
2021-04-21 09:20:54 +0700

    Improve Docker scripts

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,14 +5,14 @@ set -e # -e  Exit immediately if a command exits with a non-zero status.
 dotnet tool install --global dotnet-ef
 export PATH="$PATH:/root/.dotnet/tools"
 
-ls
-
 until dotnet ef database update --verbose; do
->&2 echo "SQL Server is starting up"
-sleep 10 # number of seconds
+>&2 echo "MySQL is starting up"
+sleep 5 # number of seconds
 done
 
 >&2 echo "MySQL is up"
 
-run_cmd="dotnet watch run --urls http://*:80"
-exec $run_cmd
+dotnet --version
+echo "$ASPNETCORE_AUTO_RELOAD_WS_ENDPOINT"
+
+dotnet watch run --urls http://*:80 --verbose --no-launch-profile
