046e705c9428b13cb967d5ed45b5dc437e50316f
ponggun <ponggun2527@gmail.com>
2021-04-23 15:16:36 +0700

    Remove unused code in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,13 +6,12 @@ dotnet tool install --global dotnet-ef
 export PATH="$PATH:/root/.dotnet/tools"
 
 until dotnet ef database update --verbose; do
->&2 echo "MySQL is starting up"
+echo "MySQL is starting up"
 sleep 5 # number of seconds
 done
 
->&2 echo "MySQL is up"
+echo "MySQL is up"
 
-dotnet --version
 echo "$ASPNETCORE_AUTO_RELOAD_WS_ENDPOINT"
 
 dotnet watch run --verbose --no-launch-profile
